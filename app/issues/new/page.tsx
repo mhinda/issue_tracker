d6bc79ea4bfa82f4c8c8a439d7a2d666a1f1a1eb
Amani Mhinda <amani.mhinda@gmail.com>
2024-04-25 10:50:59 +0300

    Refactor inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -27,6 +27,17 @@ const NewIssuePage =  () => {
   })
   const [error, setError] = useState('');
   const [isSubmitting, setSubmitting] = useState(false)
+  const onSubmit = handleSubmit(async (data) => {
+          try {
+            setSubmitting(true);
+            await axios.post('/api/issues', data)
+            router.push('/issues')
+          } catch (error) {
+            setSubmitting(false);
+            setError('An unexpexted error occurred.')
+          }
+        })
+
 
   return ( 
     <div className='max-w-xl' >
@@ -36,16 +47,7 @@ const NewIssuePage =  () => {
         </Callout.Text>
       </Callout.Root>}
       <form className='space-y-3'
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post('/api/issues', data)
-            router.push('/issues')
-          } catch (error) {
-            setSubmitting(false);
-            setError('An unexpexted error occurred.')
-          }
-        })}>
+        onSubmit={onSubmit}>
         <TextField.Root placeholder='Title' {...register('title')}>
           <TextField.Slot />
         </TextField.Root>
