4bf0b3ff418504037662b0b61ecb67914b820dcc
Amani Mhinda <amani.mhinda@gmail.com>
2024-04-26 15:50:09 +0300

    Implementind confirm delete dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,15 +1,33 @@
-import { Pencil2Icon } from '@radix-ui/react-icons'
-import { Button } from '@radix-ui/themes'
+import { TrashIcon } from '@radix-ui/react-icons'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import Link from 'next/link'
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Link href={`${issueId}/delete`}>
-      <Button color='red'>
-        <Pencil2Icon />
-          Delete Issue
-      </Button>
-    </Link>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        {/* <Link href={`${issueId}/delete`}> */}
+        <Link href='#'>
+          <Button color='red'>
+            <TrashIcon />
+              Delete Issue
+          </Button>
+        </Link>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm deletion</AlertDialog.Title>
+        <AlertDialog.Description> Are you sure you want to delete an issue.</AlertDialog.Description>
+        <Flex mt='4' gap='3'>
+          <AlertDialog.Cancel>
+            <Button>Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color='red' >Delete</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+    
   )
 }
 
