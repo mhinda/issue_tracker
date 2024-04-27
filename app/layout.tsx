5f125b587f76a39399910036cc1b69b424ec2d06
Amani Mhinda <amani.mhinda@gmail.com>
2024-04-27 19:10:17 +0300

    Configure react query

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Container, Theme } from '@radix-ui/themes';
 import "./globals.css";
 import NavBar from "./NavBar";
 import AuthProvider from './auth/Provider';
+import QueryClientProvider from './QueryClientProvider';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,14 +22,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          <Theme accentColor="blue" grayColor="sand" radius="large">
-            <NavBar />
-            <main className='p-5'>
-              <Container>{children}</Container>
-            </main>
-          </Theme> 
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme accentColor="blue" grayColor="sand" radius="large">
+              <NavBar />
+              <main className='p-5'>
+                <Container>{children}</Container>
+              </main>
+            </Theme> 
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
